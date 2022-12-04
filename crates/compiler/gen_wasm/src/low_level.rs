47e5a8f17317f67d3ca9f154e6ba52f953cdb047
Brian Carroll <brian.carroll.ireland@gmail.com>
2022-12-04 20:42:14 +0000

    gen_wasm: Implement u128 right shift by delegating to compiler_rt

--- crates/compiler/gen_wasm/src/low_level.rs
+++ crates/compiler/gen_wasm/src/low_level.rs
@@ -1716,7 +1716,7 @@ impl<'a> LowLevelCall<'a> {
                             .load_symbols(&mut backend.code_builder, &[num, bits]);
                         backend.code_builder.i64_shr_u();
                     }
-                    I128 => todo!("{:?} for I128", self.lowlevel),
+                    I128 => self.load_args_and_call_zig(backend, "__lshrti3"), // from compiler_rt
                     _ => panic_ret_type(),
                 }
             }
